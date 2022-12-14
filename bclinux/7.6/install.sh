7676bd9cc2935f801dea76d45cc7c92acd88a9d0
Vladimir Sokolovsky <vlad@nvidia.com>
2022-12-14 10:58:16 -0600

    BCLinux 7.6: Updated to DOCA_1.5.1_BSP_3.9.3
    Signed-off-by: Vladimir Sokolovsky <vlad@nvidia.com>

--- bclinux/7.6/install.sh
+++ bclinux/7.6/install.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
-# Copyright (c) 2018, Mellanox Technologies
-# All rights reserved.
+###############################################################################
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+# Copyright 2020 NVIDIA Corporation
 #
-# 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-# The views and conclusions contained in the software and documentation are those
-# of the authors and should not be interpreted as representing official policies,
-# either expressed or implied, of the FreeBSD Project.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+###############################################################################
 
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/mellanox/scripts"
+NIC_FW_UPDATE_DONE=0
 
 fspath=$(readlink -f `dirname $0`)
 
 rshimlog=`which bfrshlog 2> /dev/null`
-distro="BCLinux"
-
 log()
 {
 	if [ -n "$rshimlog" ]; then
@@ -43,51 +38,127 @@ log()
 	fi
 }
 
-# This function updates boot options. This cleans up the actual boot
-# options and installs a new boot option.
-function update_boot()
+fw_update()
 {
+	FW_UPDATER=/opt/mellanox/mlnx-fw-updater/mlnx_fw_updater.pl
+	FW_DIR=/opt/mellanox/mlnx-fw-updater/firmware/
 
-    # Update eMMC boot partitions. Update via either capsule
-    # path or default path (i.e., mlxbf-bootctl). This command
-    # MUST be executed after 'update_boot' and 'install_grub',
-    # otherwise the newly created boot option would be either
-    # cleaned up or unset from default option.
-    bfrec --bootctl --policy dual 2> /dev/null || true
-    if [ -e /lib/firmware/mellanox/boot/capsule/boot_update2.cap ]; then
-        bfrec --capsule /lib/firmware/mellanox/boot/capsule/boot_update2.cap --policy dual
-    fi
-    sleep 3
-
-    # Clean up actual boot entries.
-    bfbootmgr --cleanall
-
-    mount -t efivarfs none /sys/firmware/efi/efivars
-    efibootmgr -c -d /dev/mmcblk0 -p 1 -l "\EFI\bclinux\shimaa64-bclinux.efi" -L $distro
-    umount /sys/firmware/efi/efivars
-
-	BFCFG=`which bfcfg 2> /dev/null`
-	if [ -n "$BFCFG" ]; then
-		$BFCFG
+	if [[ -x /mnt/${FW_UPDATER} && -d /mnt/${FW_DIR} ]]; then
+		log "INFO: Updating NIC firmware..."
+		chroot /mnt ${FW_UPDATER} \
+			--force-fw-update \
+			--fw-dir ${FW_DIR}
+		if [ $? -eq 0 ]; then
+			log "INFO: NIC firmware update done"
+		else
+			log "INFO: NIC firmware update failed"
+		fi
+	else
+		log "WARNING: NIC Firmware files were not found"
 	fi
 }
 
-log "INFO: $distro installation started"
+fw_reset()
+{
+	mst start > /dev/null 2>&1 || true
+	chroot /mnt /sbin/mlnx_bf_configure > /dev/null 2>&1
 
-# Create the BCLinux partitions.
-blkdev=/dev/mmcblk0
-start_reserved=2048    # sectors required for label before first partition
-end_reserved=34        # sectors required after last partition
+	MLXFWRESET_TIMEOUT=${MLXFWRESET_TIMEOUT:-180}
+	SECONDS=0
+	while ! (chroot /mnt mlxfwreset -d /dev/mst/mt*_pciconf0 q 2>&1 | grep -w "Driver is the owner" | grep -qw "\-Supported")
+	do
+		if [ $SECONDS -gt $MLXFWRESET_TIMEOUT ]; then
+			log "INFO: NIC Firmware reset is not supported. Host power cycle is required"
+			return
+		fi
+		sleep 1
+	done
 
-disk_size=`fdisk -l $blkdev | grep "Disk $blkdev:" | awk '{print $5}'`
-disk_sectors=`fdisk -l $blkdev | grep "Disk $blkdev:" | awk '{print $7}'`
-disk_size=$((disk_size / 1024 / 1024))
-disk_start=$start_reserved
+	msg=`chroot /mnt mlxfwreset -d /dev/mst/mt*_pciconf0 -y -l 3 --sync 1 r 2>&1`
+	if [ $? -ne 0 ]; then
+		log "INFO: NIC Firmware reset failed"
+		log "INFO: $msg"
+	else
+		log "INFO: NIC Firmware reset done"
+	fi
+}
+
+bind_partitions()
+{
+	mount --bind /proc /mnt/proc
+	mount --bind /dev /mnt/dev
+	mount --bind /sys /mnt/sys
+}
+
+unmount_partitions()
+{
+	umount /mnt/sys/fs/fuse/connections > /dev/null 2>&1 || true
+	umount /mnt/sys > /dev/null 2>&1
+	umount /mnt/dev > /dev/null 2>&1
+	umount /mnt/proc > /dev/null 2>&1
+	umount /mnt/boot/efi > /dev/null 2>&1
+	umount /mnt > /dev/null 2>&1
+}
+
+#
+# Check auto configuration passed from boot-fifo
+#
+boot_fifo_path="/sys/bus/platform/devices/MLNXBF04:00/bootfifo"
+if [ -e "${boot_fifo_path}" ]; then
+	cfg_file=$(mktemp)
+	# Get 16KB assuming it's big enough to hold the config file.
+	dd if=${boot_fifo_path} of=${cfg_file} bs=4096 count=4 > /dev/null 2>&1
+
+	#
+	# Check the .xz signature {0xFD, '7', 'z', 'X', 'Z', 0x00} and extract the
+	# config file from it. Then start decompression in the background.
+	#
+	offset=$(strings -a -t d ${cfg_file} | grep -m 1 "7zXZ" | awk '{print $1}')
+	if [ -s "${cfg_file}" -a ."${offset}" != ."1" ]; then
+		log "INFO: Found bf.cfg"
+		cat ${cfg_file} | tr -d '\0' > /etc/bf.cfg
+	fi
+	rm -f $cfg_file
+fi
+
+#
+# Check PXE installation
+#
+if [ ! -e /tmp/bfpxe.done ]; then touch /tmp/bfpxe.done; bfpxe; fi
 
 PART_SCHEME="SCHEME_A"
 if [ -e /etc/bf.cfg ]; then
-	. /etc/bf.cfg
+	if ( bash -n /etc/bf.cfg ); then
+		. /etc/bf.cfg
+	else
+		log "INFO: Invalid bf.cfg"
+	fi
 fi
+
+distro="BCLinux"
+
+function_exists()
+{
+	declare -f -F "$1" > /dev/null
+	return $?
+}
+
+DHCP_CLASS_ID=${PXE_DHCP_CLASS_ID:-""}
+DHCP_CLASS_ID_OOB=${DHCP_CLASS_ID_OOB:-"NVIDIA/BF/OOB"}
+DHCP_CLASS_ID_DP=${DHCP_CLASS_ID_DP:-"NVIDIA/BF/DP"}
+FACTORY_DEFAULT_DHCP_BEHAVIOR=${FACTORY_DEFAULT_DHCP_BEHAVIOR:-"true"}
+
+if [ "${FACTORY_DEFAULT_DHCP_BEHAVIOR}" == "true" ]; then
+	# Set factory defaults
+	DHCP_CLASS_ID="NVIDIA/BF/PXE"
+	DHCP_CLASS_ID_OOB="NVIDIA/BF/OOB"
+	DHCP_CLASS_ID_DP="NVIDIA/BF/DP"
+fi
+
+log "INFO: $distro installation started"
+
+# Create the CentOS partitions.
+device=${device:-/dev/mmcblk0}
 
 SUPPORTED_SCHEMES="SCHEME_A SCHEME_B"
 if ! (echo "$SUPPORTED_SCHEMES" | grep -wq "$PART_SCHEME"); then
@@ -96,66 +167,41 @@ if ! (echo "$SUPPORTED_SCHEMES" | grep -wq "$PART_SCHEME"); then
 	PART_SCHEME="SCHEME_A"
 fi
 
+dd if=/dev/zero of=$device bs=512 count=1
+
 if [[ "${PART_SCHEME}" == "SCHEME_A" ]]; then
-	root_start=2508800
-	root_end=$((disk_sectors - end_reserved))
-	root_sectors=$((root_end - root_start))
-	disk_end=$root_end
-
-cat > /tmp/disk.sfdisk << EOF
-label: gpt
-label-id: 311E8873-97B3-4DCF-B678-CA3FCF3B59AC
-device: $blkdev
-unit: sectors
-first-lba: 34
-last-lba: $disk_end
-
-/dev/mmcblk0p1 : start=2048, size=409600, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, uuid=03E5CAD3-CE36-4ACE-A999-710E70651E06, name="EFI System Partition", bootable
-/dev/mmcblk0p2 : start=411648, size=2097152, type=EBD0A0A2-B9E5-4433-87C0-68B6B72699C7, uuid=2CE0975D-0242-449D-A53C-953EE466DC5E
-/dev/mmcblk0p3 : start=$root_start, size=$root_sectors, type=EBD0A0A2-B9E5-4433-87C0-68B6B72699C7, uuid=BE5491C2-2101-4006-B217-952DB2763B25
-EOF
+	parted --script $device -- \
+		mklabel gpt \
+		mkpart primary 1MiB 201MiB set 1 esp on \
+		mkpart primary 201MiB 1225MiB \
+		mkpart primary 1225MiB 100%
 elif [[ "${PART_SCHEME}" == "SCHEME_B" ]]; then
-	root_start=411648
-	root_sectors=10485760 # 5GB
-	root_end=$((root_start + root_sectors - 1))
-	home_start=$((root_end + 1))
-	home_sectors=14680064 # 7GB
-	home_end=$((home_start + home_sectors - 1))
-	var_start=$((home_end + 1))
-	var_end=$((disk_sectors - end_reserved))
-	var_sectors=$((var_end - var_start))
-	disk_end=$var_end
-
-cat > /tmp/disk.sfdisk << EOF
-label: gpt
-label-id: 311E8873-97B3-4DCF-B678-CA3FCF3B59AC
-device: $blkdev
-unit: sectors
-first-lba: 34
-last-lba: $disk_end
-
-/dev/mmcblk0p1 : start=2048, size=409600, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, uuid=03E5CAD3-CE36-4ACE-A999-710E70651E06, name="EFI System Partition", bootable
-/dev/mmcblk0p2 : start=$root_start, size=$root_sectors, type=EBD0A0A2-B9E5-4433-87C0-68B6B72699C7, uuid=BE5491C2-2101-4006-B217-952DB2763B25
-/dev/mmcblk0p3 : start=$home_start, size=$home_sectors, type=EBD0A0A2-B9E5-4433-87C0-68B6B72699C7, uuid=69A71056-7625-45D9-A816-3A038DAD40E1
-/dev/mmcblk0p4 : start=$var_start, size=$var_sectors, type=EBD0A0A2-B9E5-4433-87C0-68B6B72699C7, uuid=CCEFAE99-75AB-475A-939F-CFBD9A9E9747
-EOF
+	parted --script $device -- \
+		mklabel gpt \
+		mkpart primary 1MiB 201MiB set 1 esp on \
+		mkpart primary 201MiB 6000MiB \
+		mkpart primary 6000MiB 12489MiB \
+		mkpart primary 12489MiB 100%
 fi
-
-dd if=/dev/zero of=$blkdev bs=512 count=1
-
-sfdisk -f $blkdev < /tmp/disk.sfdisk
 
 sync
 
-blockdev --rereadpt "$blkdev" > /dev/null 2>&1
+partprobe "$device" > /dev/null 2>&1
+
+blockdev --rereadpt "$device" > /dev/null 2>&1
+
+if function_exists bfb_pre_install; then
+	log "INFO: Running bfb_pre_install from bf.cfg"
+	bfb_pre_install
+fi
 
 # Generate some entropy
 mke2fs  /dev/mmcblk0p2 >> /dev/null
 
 # Copy the kernel image.
-mkdosfs /dev/mmcblk0p1
-mkfs.xfs -f /dev/mmcblk0p2
-mkfs.xfs -f /dev/mmcblk0p3
+mkdosfs /dev/mmcblk0p1 -n system-boot
+mkfs.xfs -f /dev/mmcblk0p2 -L local-boot
+mkfs.xfs -f /dev/mmcblk0p3 -L writable
 if [[ "${PART_SCHEME}" == "SCHEME_B" ]]; then
 	mkfs.xfs -f /dev/mmcblk0p4
 fi
@@ -184,35 +230,15 @@ echo "Extracting /..."
 tar Jxf $fspath/image.tar.xz --warning=no-timestamp -C /mnt
 sync
 
-cd /dev/disk/by-uuid
-for uuid in *
-do
-	case `readlink -f $uuid` in
-		*mmcblk0p1)
-			UUID1=$uuid
-			;;
-		*mmcblk0p2)
-			UUID2=$uuid
-			;;
-		*mmcblk0p3)
-			UUID3=$uuid
-			;;
-		*mmcblk0p4)
-			UUID4=$uuid
-			;;
-	esac
-done
-cd -
-
 if [[ "${PART_SCHEME}" == "SCHEME_A" ]]; then
 	cat > /mnt/etc/fstab << EOF
 #
 # /etc/fstab
 #
 #
-UUID=$UUID3  /           xfs     defaults                   0 1
-UUID=$UUID2  /boot       xfs     defaults                   0 2
-UUID=$UUID1  /boot/efi   vfat    umask=0077,shortname=winnt 0 2
+/dev/mmcblk0p3  /           xfs     defaults                   0 1
+/dev/mmcblk0p2  /boot       xfs     defaults                   0 2
+/dev/mmcblk0p1  /boot/efi   vfat    umask=0077,shortname=winnt 0 2
 EOF
 elif [[ "${PART_SCHEME}" == "SCHEME_B" ]]; then
 	cat > /mnt/etc/fstab << EOF
@@ -220,10 +246,10 @@ elif [[ "${PART_SCHEME}" == "SCHEME_B" ]]; then
 # /etc/fstab
 #
 #
-UUID=$UUID2  /           xfs     defaults                   0 1
-UUID=$UUID3  /home       xfs     defaults                   0 1
-UUID=$UUID4  /var        xfs     defaults                   0 1
-UUID=$UUID1  /boot/efi   vfat    umask=0077,shortname=winnt 0 2
+/dev/mmcblk0p2  /           xfs     defaults                   0 1
+/dev/mmcblk0p3  /home       xfs     defaults                   0 2
+/dev/mmcblk0p4  /var        xfs     defaults                   0 2
+/dev/mmcblk0p1  /boot/efi   vfat    umask=0077,shortname=winnt 0 2
 EOF
 fi
 
@@ -261,9 +287,7 @@ chmod 600 /mnt/etc/ssh/*
 /bin/rm -f /mnt/etc/systemd/system/multi-user.target.wants/firewalld.service
 /bin/rm -f /mnt/etc/systemd/system/dbus-org.fedoraproject.FirewallD1.service
 
-mount --bind /proc /mnt/proc
-mount --bind /dev /mnt/dev
-mount --bind /sys /mnt/sys
+bind_partitions
 
 /bin/rm -f /mnt/boot/vmlinux-*.bz2
 
@@ -271,19 +295,24 @@ mount --bind /sys /mnt/sys
 # parameters, and append the root filesystem parameters.
 bootarg="$(cat /proc/cmdline | sed 's/initrd=initramfs//;s/console=.*//')"
 sed -i -e "s@GRUB_CMDLINE_LINUX=.*@GRUB_CMDLINE_LINUX=\"crashkernel=auto $bootarg console=hvc0 console=ttyAMA0 earlycon=pl011,0x01000000 modprobe.blacklist=mlx5_core,mlx5_ib\"@" /mnt/etc/default/grub
+if (lspci -n -d 15b3: | grep -wq 'a2dc'); then
+    # BlueField-3
+    sed -i -e "s/0x01000000/0x13010000/g" /mnt/etc/default/grub
+fi
 
 chroot /mnt grub2-mkconfig -o /boot/efi/EFI/bclinux/grub.cfg
 
 kdir=$(/bin/ls -1d /mnt/lib/modules/4.18* /mnt/lib/modules/4.19* /mnt/lib/modules/4.20* /mnt/lib/modules/5.4* 2> /dev/null)
 kver=""
 if [ -n "$kdir" ]; then
-	kver=${kdir##*/}
-	chroot /mnt grub2-set-default 0
+    kver=${kdir##*/}
+    DRACUT_CMD=`chroot /mnt /bin/ls -1 /sbin/dracut /usr/bin/dracut 2> /dev/null | head -1`
+    chroot /mnt grub2-set-default 0
+    chroot /mnt $DRACUT_CMD --kver ${kver} --force --add-drivers "dw_mmc-bluefield dw_mmc dw_mmc-pltfm mmc_block mlxbf_tmfifo virtio_console" /boot/initramfs-${kver}.img
 else
-	kver=$(/bin/ls -1 /mnt/lib/modules/ | head -1)
+    kver=$(/bin/ls -1 /mnt/lib/modules/ | head -1)
 fi
 
-chroot /mnt /usr/bin/mkinitrd --force --with=xfs --with=dw_mmc-bluefield --with=dw_mmc-pltfm --with=dw_mmc --with=mtd_blkdevs --with=mmc_block --with=mlxbf_gige --with=mlxbf-tmfifo --with=mlx5_ib /boot/initramfs-${kver}.img $kver
 
 echo bclinux | chroot /mnt passwd root --stdin
 
@@ -311,10 +340,7 @@ p1m0_uuid=`$UUIDGEN`
 p0m0_mac=`echo ${p0m0_uuid} | sed -e 's/-//;s/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/'`
 p1m0_mac=`echo ${p1m0_uuid} | sed -e 's/-//;s/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/'`
 
-pciids=`/usr/sbin/lspci -nD -d 15b3:a2d2 2> /dev/null | awk '{print $1}'`
-if [ ! -n "$pciids" ]; then
-	pciids=`/usr/sbin/lspci -nD -d 15b3:a2d6 2> /dev/null | awk '{print $1}'`
-fi
+pciids=`lspci -nD 2> /dev/null | grep 15b3:a2d[26c] | awk '{print $1}'`
 
 mkdir -p /mnt/etc/mellanox
 echo > /mnt/etc/mellanox/mlnx-sf.conf
@@ -331,15 +357,26 @@ EOF
 done
 
 # Update HW-dependant files
-if (/usr/sbin/lspci -n -d 15b3: | grep -wq 'a2d2'); then
+if (lspci -n -d 15b3: | grep -wq 'a2d2'); then
 	# BlueField-1
 	ln -snf snap_rpc_init_bf1.conf /mnt/etc/mlnx_snap/snap_rpc_init.conf
 	# OOB interface does not exist on BlueField-1
 	/bin/rm -f /mnt/etc/sysconfig/network-scripts/ifcfg-oob_net0
-elif (/usr/sbin/lspci -n -d 15b3: | grep -wq 'a2d6'); then
+elif (lspci -n -d 15b3: | grep -wq 'a2d6'); then
 	# BlueField-2
 	ln -snf snap_rpc_init_bf2.conf /mnt/etc/mlnx_snap/snap_rpc_init.conf
+elif (lspci -n -d 15b3: | grep -wq 'a2dc'); then
+	# BlueField-3
+	chroot /mnt rpm -e mlnx-snap || true
 fi
+
+	mkdir -p /mnt/etc/dhcp
+	cat >> /mnt/etc/dhcp/dhclient.conf << EOF
+send vendor-class-identifier "$DHCP_CLASS_ID_DP";
+interface "oob_net0" {
+  send vendor-class-identifier "$DHCP_CLASS_ID_OOB";
+}
+EOF
 
 # Customisations per PSID
 FLINT=""
@@ -365,6 +402,13 @@ if [ -n "$FLINT" ]; then
 	esac
 fi
 
+if [ "$WITH_NIC_FW_UPDATE" == "yes" ]; then
+	if [ $NIC_FW_UPDATE_DONE -eq 0 ]; then
+		fw_update
+		NIC_FW_UPDATE_DONE=1
+	fi
+fi
+
 # Clean up logs
 echo > /mnt/var/log/messages
 echo > /mnt/var/log/maillog
@@ -373,6 +417,11 @@ echo > /mnt/var/log/firewalld
 echo > /mnt/var/log/audit/audit.log
 /bin/rm -f /mnt/var/log/yum.log
 /bin/rm -rf /mnt/tmp/*
+
+if function_exists bfb_modify_os; then
+	log "INFO: Running bfb_modify_os from bf.cfg"
+	bfb_modify_os
+fi
 
 sync
 
@@ -391,12 +440,84 @@ umount /mnt
 
 sync
 
-# Update the firmware and boot the current kernel image.
-update_boot
+bfrec --bootctl --policy dual 2> /dev/null || true
+if [ -e /lib/firmware/mellanox/boot/capsule/boot_update2.cap ]; then
+	bfrec --capsule /lib/firmware/mellanox/boot/capsule/boot_update2.cap --policy dual
+fi
+
+
+# Clean up actual boot entries.
+bfbootmgr --cleanall > /dev/null 2>&1
+
+mount -t efivarfs none /sys/firmware/efi/efivars
+/bin/rm -f /sys/firmware/efi/efivars/Boot* > /dev/null 2>&1
+efibootmgr -c -d /dev/mmcblk0 -p 1 -l "\EFI\bclinux\shimaa64-bclinux.efi" -L $distro
+umount /sys/firmware/efi/efivars
+
+BFCFG=`which bfcfg 2> /dev/null`
+if [ -n "$BFCFG" ]; then
+	# Create PXE boot entries
+	if [ -e /etc/bf.cfg ]; then
+		mv /etc/bf.cfg /etc/bf.cfg.orig
+	fi
+
+	cat > /etc/bf.cfg << EOF
+BOOT0=DISK
+BOOT1=NET-NIC_P0-IPV4
+BOOT2=NET-NIC_P0-IPV6
+BOOT3=NET-NIC_P1-IPV4
+BOOT4=NET-NIC_P1-IPV6
+BOOT5=NET-OOB-IPV4
+BOOT6=NET-OOB-IPV6
+PXE_DHCP_CLASS_ID=$DHCP_CLASS_ID
+EOF
+
+	$BFCFG
+
+	# Restore the original bf.cfg
+	/bin/rm -f /etc/bf.cfg
+	if [ -e /etc/bf.cfg.orig ]; then
+		grep -v PXE_DHCP_CLASS_ID= /etc/bf.cfg.orig > /etc/bf.cfg
+	fi
+fi
+
+if [ -n "$BFCFG" ]; then
+	$BFCFG
+fi
 
 echo
 echo "ROOT PASSWORD is \"bclinux\""
 echo
 
+if function_exists bfb_post_install; then
+	log "INFO: Running bfb_post_install from bf.cfg"
+	bfb_post_install
+fi
+
 log "INFO: Installation finished"
+
+if [ "$WITH_NIC_FW_UPDATE" == "yes" ]; then
+	if [ $NIC_FW_UPDATE_DONE -eq 1 ]; then
+		log "INFO: Running NIC Firmware reset"
+		if [ "X$mode" == "Xmanufacturing" ]; then
+			log "INFO: Rebooting..."
+		fi
+		# Wait for these messages to be pulled by the rshim service
+		# as mlxfwreset will restart the DPU
+		sleep 3
+		# Reset NIC FW
+		if [[ "${PART_SCHEME}" == "SCHEME_A" ]]; then
+			mount /dev/mmcblk0p3 /mnt
+		else
+			mount /dev/mmcblk0p2 /mnt
+		fi
+		bind_partitions
+		fw_reset
+		unmount_partitions
+	fi
+fi
+
+sleep 3
 log "INFO: Rebooting..."
+# Wait for these messages to be pulled by the rshim service
+sleep 3
